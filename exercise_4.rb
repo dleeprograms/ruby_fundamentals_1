a4bc75e1a27ce4c1a59ba42b355efa386ce3b826
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-12 17:57:48 -0500

    exercise_4_finished_with_question_of_==0

---
+++ exercise_4.rb
@@ -0,0 +1,21 @@
+one_to_hundred = (1..100)
+
+one_to_hundred.each do |num|
+
+
+
+  if num%3 == 0
+    puts "Bit"
+
+  elsif num%5 == 0
+    puts "Maker"
+
+  else num%3 == 0 && num%5 == 0
+     puts "BitMaker"
+
+  puts num
+
+  end
+
+
+end
