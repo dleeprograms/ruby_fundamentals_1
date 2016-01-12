d0ff7f4cd170d69d69b7a5bd01dc37234083cda0
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-12 16:15:49 -0500

    exercise_2_question_4

--- exercise_2.rb
+++ exercise_2.rb
@@ -2,4 +2,6 @@
 
 # puts "4" + 4 # Stringed interger + interger
 
-puts "#{45628 * 7839}"
+# puts "#{45628 * 7839}" # String interpolation
+
+puts (10 < 20 && 30 < 20) || !(10 == 11) # true or false... I guess it is true.
